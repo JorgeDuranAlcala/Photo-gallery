1dc6278e17a21029aac89808a8fc3cf51013f9f6
Jorgeluis <jorgeluis21.duran@gmail.com>
2019-09-19 22:09:04 -0400

    navigation and routes done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PhotoListComponent } from './components/photo-list/photo-list.component';
+import { PhotoFormComponent } from './components/photo-form/photo-form.component';
+import { PhotoPreviewComponent } from './components/photo-preview/photo-preview.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component: HomeComponent },
+  { path:'photos', component: PhotoListComponent },
+  { path:'addPhotos', component: PhotoFormComponent },
+  { path:'previewPhoto', component: PhotoPreviewComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { HomeComponent } from './components/home/home.component';
+import { PhotoListComponent } from './components/photo-list/photo-list.component';
+import { PhotoFormComponent } from './components/photo-form/photo-form.component';
+import { PhotoPreviewComponent } from './components/photo-preview/photo-preview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HomeComponent,
+    PhotoListComponent,
+    PhotoFormComponent,
+    PhotoPreviewComponent
   ],
   imports: [
     BrowserModule,
